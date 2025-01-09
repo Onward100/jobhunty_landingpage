6eede4303eec5625a234cdcb87da66830e24ee95
Woruka Onward Onyekachi <worukaonward@gmail.com>
2025-01-09 20:40:11 +0100

    first commit

---
+++ src/components/FeaturedJobs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FeaturedJobs() {
+  return (
+    <div>FeaturedJobs</div>
+  )
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <>
+      <nav className='flex items-center justify-between p-2'>
+    <div>
+      Navbar
+    </div>
+    <div>
+      Nav
+    </div>
+      </nav>
+    </>
+  )
+}
