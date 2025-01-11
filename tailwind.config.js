55b6d4e2ec34293511caebe580e905ee53c5f304
Woruka Onward Onyekachi <worukaonward@gmail.com>
2025-01-11 05:08:18 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
       colors: {
         customGreen: '#56CDAD',
       },
+      fontFamily: {
+        roboto: ['"Roboto"', 'sans-serif'],
+      },
     },
   },
   plugins: [],
